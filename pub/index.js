24754081a60a82eee041ea5f785b22bb1b126e79
R. A. Souza <alves.wm@gmail.com>
2020-10-15 22:08:54 -0300

    adds linter and live reload

--- pub/index.js
+++ pub/index.js
@@ -1,9 +1,31 @@
 const { PubSub } = require('@google-cloud/pubsub')
 
+const ALREADY_EXISTS = 6
+
 const client = new PubSub()
 const listTopics = async () => {
-  const topics = await client.getTopics()
+  const [topics] = await client.getTopics()
   console.log(topics)
 }
 
-listTopics()
+const createTopic = async topic => {
+  try {
+    await client.createTopic(topic)
+    console.log(`Topic ${topic} created`)
+  } catch (error) {
+    if (error.code === ALREADY_EXISTS) console.warn(error)
+  }
+}
+
+const publish = async (topic, message) => {
+  const myTopic = client.topic(topic)
+  // console.log(myTopic)
+}
+
+createTopic('test')
+publish('test', null)
+
+process.on('unhandledRejection', err => {
+  console.error(err.message)
+  process.exitCode = 1
+})
